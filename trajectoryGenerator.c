f164f094d476760ce1b239dd41f4c702e86c1d5a
lucascqr <lucas.coquard@gmail.com>
2024-05-05 17:07:11 +0200

    V2

--- trajectoryGenerator.c
+++ trajectoryGenerator.c
@@ -1,12 +1,21 @@
-@ -1,200 +1,135 @@
+@ -1,145 +1,197 @@
 #include <math.h>
+#include <stdlib.h>
 #include "trajectoryGenerator.h"
 #include "timer.h"
 #include "Robot.h"
 #include "utilities.h"
+#include "UART_Protocol.h"
+#include "QEI.h"
+
 
 volatile GhostPosition ghostPosition;
 static unsigned long lastUpdateTime = 0;
+double thetaRobot;
+int newPos = 0;
+
+double maxAngularSpeed = 2*PI;
+double angularAccel = 2*PI;
 
 void InitTrajectoryGenerator(void) {
     ghostPosition.x = 0.0;
@@ -23,114 +32,84 @@ void InitTrajectoryGenerator(void) {
 
 void UpdateTrajectory() // Mise a jour de la trajectoire en fonction de l'etat actuel
 {
-    switch (ghostPosition.state) {
-        case IDLE:
-            ghostPosition.linearSpeed = 0.0;
-            ghostPosition.angularSpeed = 0.0;
-
-            if (ghostPosition.targetX != 0.0 || ghostPosition.targetY != 0.0) // Verifier si une nouvelle cible est definie
+    ///Calcul de quelques variables interm��diaires qui pourraient servir...
+    // Angle de la cible
+    double targetAngle = atan2(ghostPosition.targetY - ghostPosition.y, ghostPosition.targetX - ghostPosition.x);
+    double angleAParcourir = ModuloByAngle(ghostPosition.theta, targetAngle - ghostPosition.theta);
+    double angleArret = ghostPosition.angularSpeed*ghostPosition.angularSpeed/(2*angularAccel);
+        
+    if(angleAParcourir != 0) ///On doit tourner
+    {
+        //Soit l'angle � parcourir est positif 
+        if(angleAParcourir>0)
+        {
+            //Soit l'angle � parcourir est sup�rieur � la distance d'arr�t
+            if(angleAParcourir > angleArret)
             {
-                // Determiner l'angle actuel du robot par rapport a la cible
-                double angleToTarget = atan2(ghostPosition.targetY - ghostPosition.y, ghostPosition.targetX - ghostPosition.x);
-                double angleDifference = ModuloByAngle(ghostPosition.theta, angleToTarget - ghostPosition.theta);
-
-                // Si le robot n'est pas axe vers la cible, commencer par tourner
-                if (fabs(angleDifference) > ANGLE_TOLERANCE) {
-                    ghostPosition.state = ROTATING;
-                } else {
-                    // Si le robot est deja oriente vers la cible, passer directement a l'avancement
-                    ghostPosition.state = ADVANCING;
+                //Soit on a d�j� atteint la Vmax angulaire
+                if(ghostPosition.angularSpeed >= maxAngularSpeed)
+                {
+                    //On maintient la vitesse
+                }
+                else
+                {
+                    //Soit on ne l'a pas atteint
+                    //On acc�l�re avec saturation � VMax
+                    ghostPosition.angularSpeed = Min(ghostPosition.angularSpeed + angularAccel /FREQ_ECH_QEI, maxAngularSpeed);
                 }
             }
-            break;
-
-        case ROTATING:
-            RotateTowardsTarget(timestamp);
-            break;
-
-        case ADVANCING:
-            AdvanceTowardsTarget(timestamp);
-            break;
+            //Soit l'angle � parcourir est inf�rieur
+            else                
+            {
+                //On freine
+                ghostPosition.angularSpeed = Max(ghostPosition.angularSpeed - angularAccel /FREQ_ECH_QEI, 0);               
+            }            
+        }       
+        
+        //Soit l'angle � parcourir est n�gatif 
+        else
+        {
+            //Soit l'angle � parcourir est sup�rieur � la distance d'arr�t
+            if(abs(angleAParcourir) > angleArret)
+            {
+                //Soit on a d�j� atteint la Vmax angulaire
+                if(ghostPosition.angularSpeed <= -maxAngularSpeed)
+                {
+                    //On maintient la vitesse
+                }
+                else
+                {
+                    //Soit on ne l'a pas atteint
+                    //On acc�l�re avec saturation � VMax
+                    ghostPosition.angularSpeed = Max(ghostPosition.angularSpeed - angularAccel /FREQ_ECH_QEI, -maxAngularSpeed);
+                }
+            }
+            //Soit l'angle � parcourir est inf�rieur
+            else                
+            {
+                //On freine
+                ghostPosition.angularSpeed = Min(ghostPosition.angularSpeed + angularAccel /FREQ_ECH_QEI, 0);               
+            }     
+        }
+        
+        ghostPosition.theta += ghostPosition.angularSpeed / FREQ_ECH_QEI;
+        //Si la nouvelle vitesse angulaire est nulle ici
+        //On a termin� la rotation, l'angle du ghost est donc l'angle de la cible (on casse les erreurs d'arrondi d'int�gration)
+        if(ghostPosition.angularSpeed==0)
+        {
+            ghostPosition.theta = targetAngle;
+        }
     }
-    lastUpdateTime = timestamp;
     robotState.consigneLin = ghostPosition.linearSpeed;
     robotState.consigneAng = ghostPosition.angularSpeed;
 }
 
-void RotateTowardsTarget(double currentTime) // Orientation du Ghost vers le waypoint
-{
-    double deltaTime = (currentTime - lastUpdateTime) / 1000.0;
-    double thetaWaypoint = atan2(ghostPosition.targetY - ghostPosition.y, ghostPosition.targetX - ghostPosition.x);
-    double thetaRestant = thetaWaypoint - ModuloByAngle(thetaWaypoint, ghostPosition.theta);
-    double thetaArret = pow(ghostPosition.angularSpeed, 2) / (2 * MAX_ANGULAR_ACCEL);
-
-    int shouldAccelerate = 0;
-    int isDirectionPositive = thetaRestant > 0 ? 1 : 0;
-
-    if (fabs(thetaRestant) < ANGLE_TOLERANCE) {
-        ghostPosition.state = ADVANCING;
-        ghostPosition.angularSpeed = 0;
-        return;
-    }
-
-    if (isDirectionPositive) {
-        shouldAccelerate = (ghostPosition.angularSpeed < MAX_ANGULAR_SPEED && thetaRestant > thetaArret) ? 1 : 0;
-    } else {
-        shouldAccelerate = (ghostPosition.angularSpeed > -MAX_ANGULAR_SPEED && fabs(thetaRestant) > thetaArret) ? 1 : 0;
-    }
-
-    if (shouldAccelerate) {
-        ghostPosition.angularSpeed += (isDirectionPositive ? 1 : -1) * MAX_ANGULAR_ACCEL * deltaTime;
-    } else if (fabs(thetaRestant) <= thetaArret || (!isDirectionPositive && ghostPosition.angularSpeed > 0) || (isDirectionPositive && ghostPosition.angularSpeed < 0)) {
-        ghostPosition.angularSpeed -= (isDirectionPositive ? 1 : -1) * MAX_ANGULAR_ACCEL * deltaTime;
-    }
-
-    ghostPosition.angularSpeed = fmin(fmax(ghostPosition.angularSpeed, -MAX_ANGULAR_SPEED), MAX_ANGULAR_SPEED);
-    ghostPosition.theta += ghostPosition.angularSpeed * deltaTime;
-    ghostPosition.theta = ModuloByAngle(0, ghostPosition.theta);
-
-    ghostPosition.angleToTarget = thetaRestant;
-}
-
-void AdvanceTowardsTarget(double currentTime) // Avancement lineaire du Ghost vers le waypoint
-{
-    double deltaTime = (currentTime - lastUpdateTime) / 1000.0;
-    double distance = DistanceProjete(ghostPosition.x, ghostPosition.y, 
-                                      ghostPosition.x + cos(ghostPosition.theta),  ghostPosition.y + sin(ghostPosition.theta),
-                                      ghostPosition.targetX, ghostPosition.targetY); // distance par rapport au projett� sur la droite de direction
-
-    if (distance < DISTANCE_TOLERANCE) {
-        ghostPosition.state = IDLE;
-        ghostPosition.linearSpeed = 0.0;
-        return;
-    }
-
-    double accelDistance = (MAX_LINEAR_SPEED * MAX_LINEAR_SPEED - ghostPosition.linearSpeed * ghostPosition.linearSpeed) / (2 * MAX_LINEAR_ACCEL);
-    double decelDistance = (ghostPosition.linearSpeed * ghostPosition.linearSpeed) / (2 * MAX_LINEAR_ACCEL);
-
-    if (distance <= (decelDistance + DISTANCE_TOLERANCE)) {
-        // Phase de deceleration
-        ghostPosition.linearSpeed = (MAX_LINEAR_SPEED * distance) / decelDistance;
-    } else if (distance > decelDistance + accelDistance) {
-        ghostPosition.linearSpeed += MAX_LINEAR_ACCEL * deltaTime;
-        ghostPosition.linearSpeed = fmin(ghostPosition.linearSpeed, MAX_LINEAR_SPEED);
-    } else {
-        double vMedian = sqrt(MAX_LINEAR_ACCEL * distance + ghostPosition.linearSpeed / 2);
-        ghostPosition.linearSpeed += MAX_LINEAR_ACCEL * deltaTime;
-        ghostPosition.linearSpeed = fmin(ghostPosition.linearSpeed, vMedian);
-    }
-
-    ghostPosition.distanceToTarget = distance;
-
-    // Deplacer le robot vers le waypoint
-    ghostPosition.x += ghostPosition.linearSpeed * cos(ghostPosition.theta) * deltaTime;
-    ghostPosition.y += ghostPosition.linearSpeed * sin(ghostPosition.theta) * deltaTime;
-}
-
 void SendGhostData() {
-    unsigned char ghostPayload[8];
+    unsigned char ghostPayload[16];
     getBytesFromFloat(ghostPayload, 0, (float) ghostPosition.angleToTarget);
     getBytesFromFloat(ghostPayload, 4, (float) ghostPosition.distanceToTarget);
-
-    UartEncodeAndSendMessage(GHOST_DATA, 8, ghostPayload);
+    getBytesFromFloat(ghostPayload, 8, (float) ghostPosition.theta);
+    getBytesFromFloat(ghostPayload, 12, (float) ghostPosition.angularSpeed);
+    
+    UartEncodeAndSendMessage(GHOST_DATA, 16, ghostPayload);
 }
